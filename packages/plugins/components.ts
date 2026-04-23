a79a5fd57e027851b26c8e2b3d640b393513bd3c
君惜 <xinlei3166@126.com>
2026-04-23 17:51:15 +0800

    feat: 为搜索表格组件添加工具栏插槽和搜索属性配置

--- packages/plugins/components.ts
+++ packages/plugins/components.ts
@@ -1,9 +1,13 @@
+import DialogHeader from '@packages/components/dialog-header/index.vue'
+import EllipsisTooltip from '@packages/components/ellipsis-tooltip/index.vue'
 import Phone from '@packages/components/phone/index.vue'
 import Search from '@packages/components/search/index.vue'
 import SearchTable from '@packages/components/table'
 
 export default {
   install: (app: any) => {
+    app.component('DialogHeader', DialogHeader)
+    app.component('EllipsisTooltip', EllipsisTooltip)
     app.component('Phone', Phone)
     app.component('Search', Search)
     app.component('SearchTable', SearchTable)
